5544507463a31d2d78483aef16d69c4300aae85b
DarkGeekMS <mohamedshawky911@gmail.com>
2021-05-30 02:23:46 +0200

    add pseudocode for function approximation DUE

--- due_fa.py
+++ due_fa.py
@@ -1,4 +1,18 @@
 """ Solves Direct Utility Estimation (DUE) Using Function Approximation (FA) """
 
+from utils import perform_single_trial
+
+
+def update_params(trial_results, prev_params):
+    # update function approximator parameters using gradient descent
+    pass
+
+
 def solve_fa(grid, terminals, policy):
+    # solve direct utility estimation using function approximation
+    # randomly initialize parameters list
+    # loop for a large number of trials
+        # perform a single trial
+        # update parameters using gradient descent
+    # print out estimated utilities
     pass
